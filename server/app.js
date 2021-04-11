0d0cdda2422e1cf34a5e36801b5ecefcf8c10890
dedeco99 <dedeco99@gmail.com>
2021-04-11 12:52:41 +0100

    Added build references

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const path = require("path");
 const socket = require("socket.io");
 
 const app = express();
 
 app.set("port", process.env.PORT || 5000);
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*/", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/build/index.html`));
+});
 
 const server = app.listen(app.get("port"), () => {
   console.log("Listening on port", app.get("port"));
